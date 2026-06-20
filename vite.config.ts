3b36a12527feeb0c2a48b25e9d1c73395d2da94e
simoganger <simonsoft7@gmail.com>
2026-06-20 19:56:10 -0400

    Updated vite config to remove localhost proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4174',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: '/'
 });
