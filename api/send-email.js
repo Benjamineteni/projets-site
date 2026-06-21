9036f31e820a8c7543979a433a06cf7bd422a683
Eteni elat benjamin <benjamineteni@gmail.com.com>
2026-06-21 16:10:15 +0100

    ajout du nom de domaine

--- api/send-email.js
+++ api/send-email.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
 
   try {
     const result = await resend.emails.send({
-      from: 'Fondation Coeur Mère <contact@fondation-coeur-mere.com>',
+      from: 'no-reply@fondation-coeur-mere.com',
       to: emailTo,
       subject: `Nouvelle demande de retrait ${platform}`,
       html,
