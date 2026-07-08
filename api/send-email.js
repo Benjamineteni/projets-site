99054490d522eb3ce60e6ea61470c0acdee5987e
Eteni elat benjamin <benjamineteni@gmail.com.com>
2026-07-08 07:55:53 +0100

    nouveaux commit

--- api/send-email.js
+++ api/send-email.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
 
   try {
     const result = await resend.emails.send({
-      from: 'no-reply@fondation-coeur-mere.com',
+      from: 'no-reply@fondationcoeurmere1@gmail.com',
       to: emailTo,
       subject: `Nouvelle demande de retrait ${platform}`,
       html,
